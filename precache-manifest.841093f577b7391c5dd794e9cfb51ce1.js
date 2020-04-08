f6bdf11a8ba47cb801eabc71954c2d97a657c3a4
imranhayat <imran.hayat92@gmail.com>
2020-04-08 17:52:11 +0500

    Updates

---
+++ precache-manifest.841093f577b7391c5dd794e9cfb51ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ba4d6808b375aa51a6dd18a37c38b7",
+    "url": "/employees/index.html"
+  },
+  {
+    "revision": "fe3a3705cc4bee321e4b",
+    "url": "/employees/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b4d4c71a21f56a6f44f1",
+    "url": "/employees/static/js/2.4b50f5a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employees/static/js/2.4b50f5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3a3705cc4bee321e4b",
+    "url": "/employees/static/js/main.89def02b.chunk.js"
+  },
+  {
+    "revision": "48d159d5eec304ff8180",
+    "url": "/employees/static/js/runtime-main.332a3cec.js"
+  }
+]);
